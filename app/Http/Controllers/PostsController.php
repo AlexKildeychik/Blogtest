3c4cb66827f578ec56655d05f6bd2f45b63ee722
Кильдейчик Александр < >
2017-07-19 22:31:07 +0200

    Registration system added.

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -7,6 +7,11 @@ use App\Post;
 
 class PostsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth')->except(['index', 'show']);
+    }
+
     public function index()
     {
         $posts = Post::latest()->get();
@@ -25,7 +30,11 @@ class PostsController extends Controller
         $this->validate(request(),[
             'title' => 'required',
             'body' => 'required',]);
-        Post::create(request(['title', 'body']));
+
+
+        auth()->user()->publish(
+            new Post(request(['title', 'body']))
+        );
 
         return redirect('/');
     }
--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,27 @@
 */
 
 
-Route::get('/', 'PostsController@index');
+Route::get('/', 'PostsController@index')->name('home');
+
 Route::get('/posts/create', 'PostsController@create');
+
 Route::post('/posts', 'PostsController@store');
+
 Route::get('/posts/{post}', 'PostsController@show');
+
 Route::post('/posts/{post}/comments', 'CommentsController@store');
+
+Route::get('/register', 'RegistrationController@create');
+
+Route::post('/register', 'RegistrationController@store');
+
+Route::get('/login', 'SessionsController@create');
+
+Route::post('/login', 'SessionsController@store');
+
+Route::get('/logout', 'SessionsController@destroy');
+
+
 /*Route::get('/news', 'NewsController@index');
 Route::get('/news/{article}', 'NewsController@show');*/
 
